744d955f5c95d15f7bff73a9abd5262fd2696ba2
Christof Dallermassl <christof@dallermassl.at>
2013-11-19 00:35:59 +0100

    added first version of french

---
+++ js/fr_small.js
@@ -0,0 +1,194 @@
+/* add link for this design to switch element */
+$(function () {
+  addSwitcherFrSmall();
+});
+
+function addSwitcherFrSmall() {	
+  var switchElement = document.getElementById("switcherLanguage");
+  var aTag = document.createElement('a');
+  aTag.setAttribute('href', "#");
+  aTag.setAttribute('onclick', "initFrSmall(); return false;");
+  aTag.innerHTML = "Francais";
+	var myDiv = document.createElement('div');
+  myDiv.appendChild(aTag);
+	switchElement.appendChild(myDiv);
+}
+
+/* minutes definitions */
+function initFrSmall() {
+	minutes = new Array();
+	minutes[00] = "il est hour";
+	minutes[05] = "il est hour oclock et1 cinqu";
+	minutes[10] = "il est hour oclock et1 dix";
+	minutes[15] = "il est hour oclock et1 quart";
+	minutes[20] = "il est hour oclock et1 quart";
+	minutes[25] = "il est hour oclock vingtcinqu";
+	minutes[30] = "il est hour oclock et2 demi";
+	minutes[35] = "il est hourPlusOne moins vingtcinqu";
+	minutes[40] = "il est hourPlusOne moins vingt";
+	minutes[45] = "il est hourPlusOne oclock moins le quart";
+	minutes[50] = "il est hourPlusOne oclock moins dix";
+	minutes[55] = "il est hourPlusOne oclock moins cinqu";
+	
+	hours = new Array();
+	hours[00] = "minuit";
+	hours[12] = "midi";
+
+addDataToTableFrSmall('mainclock');
+updateWordClock();
+}
+
+function addDataToTableFrSmall(tableId) {
+  tabBody = document.getElementById(tableId);
+	tabBody.innerHTML = "";
+  addRow(tabBody,
+		new Array(
+      createColumn("il", "I"),
+      createColumn("il", "L"),
+      createColumn("", "N"),
+      createColumn("est", "E"),
+      createColumn("est", "S"),
+      createColumn("est", "T"),
+      createColumn("", "O"),
+      createColumn("twoHour", "D"),
+      createColumn("twoHour", "E"),
+      createColumn("twoHour", "U"),
+      createColumn("twoHour", "X")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("fourHour", "Q"),
+      createColumn("fourHour", "U"),
+      createColumn("fourHour", "A"),
+      createColumn("fourHour", "T"),
+      createColumn("fourHour", "R"),
+      createColumn("fourHour", "E"),
+      createColumn("threeHour", "T"),
+      createColumn("threeHour", "R"),
+      createColumn("threeHour", "O"),
+      createColumn("threeHour", "I"),
+      createColumn("threeHour", "S")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("nineHour", "N"),
+      createColumn("nineHour", "E"),
+      createColumn("nineHour", "U"),
+      createColumn("nineHour", "F"),
+      createColumn("oneHour", "U"),
+      createColumn("oneHour", "N"),
+      createColumn("oneHour", "E"),
+      createColumn("sevenHour", "S"),
+      createColumn("sevenHour", "E"),
+      createColumn("sevenHour", "P"),
+      createColumn("sevenHour", "T")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("eightHour", "H"),
+      createColumn("eightHour", "U"),
+      createColumn("eightHour", "I"),
+      createColumn("eightHour", "T"),
+      createColumn("sixHour", "S"),
+      createColumn("sixHour", "I"),
+      createColumn("sixHour", "X"),
+      createColumn("fiveHour", "C"),
+      createColumn("fiveHour", "I"),
+      createColumn("fiveHour", "N"),
+      createColumn("fiveHour", "Q")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("midi", "M"),
+      createColumn("midi", "I"),
+      createColumn("midi", "D"),
+      createColumn("midi", "I"),
+      createColumn("", "X"),
+      createColumn("minuit", "M"),
+      createColumn("minuit", "I"),
+      createColumn("minuit", "N"),
+      createColumn("minuit", "U"),
+      createColumn("minuit", "I"),
+      createColumn("minuit", "T")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("elevenHour", "O"),
+      createColumn("elevenHour", "N"),
+      createColumn("elevenHour", "Z"),
+      createColumn("elevenHour", "E"),
+      createColumn("", "R"),
+      createColumn("oclock", "H"),
+      createColumn("oclock", "E"),
+      createColumn("oclock", "U"),
+      createColumn("oclock", "R"),
+      createColumn("oclock", "E"),
+      createColumn("oclock", "S")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("moins", "M"),
+      createColumn("moins", "O"),
+      createColumn("moins", "I"),
+      createColumn("moins", "N"),
+      createColumn("moins", "S"),
+      createColumn("", "O"),
+      createColumn("le", "L"),
+      createColumn("le", "E"),
+      createColumn("dix", "D"),
+      createColumn("dix", "I"),
+      createColumn("dix", "X")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("et1", "E"),
+      createColumn("et1", "T"),
+      createColumn("", "R"),
+      createColumn("quart", "Q"),
+      createColumn("quart", "U"),
+      createColumn("quart", "A"),
+      createColumn("quart", "R"),
+      createColumn("quart", "T"),
+      createColumn("", "P"),
+      createColumn("", "M"),
+      createColumn("", "D")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("vingt vingtcinqu", "V"),
+      createColumn("vingt vingtcinqu", "I"),
+      createColumn("vingt vingtcinqu", "N"),
+      createColumn("vingt vingtcinqu", "G"),
+      createColumn("vingt vingtcinqu", "T"),
+      createColumn("vingtcinqu", "-"),
+      createColumn("cinqu vingtcinqu", "C"),
+      createColumn("cinqu vingtcinqu", "I"),
+      createColumn("cinqu vingtcinqu", "N"),
+      createColumn("cinqu vingtcinqu", "Q"),
+      createColumn("cinqu vingtcinqu", "U")
+		)
+	);
+  addRow(tabBody,
+		new Array(
+      createColumn("et2", "E"),
+      createColumn("et2", "T"),
+      createColumn("", "S"),
+      createColumn("demi", "D"),
+      createColumn("demi", "E"),
+      createColumn("demi", "M"),
+      createColumn("demi", "I"),
+      createColumn("demi", "E"),
+      createColumn("", "P"),
+      createColumn("", "A"),
+      createColumn("", "M")
+		)
+	);
+}
